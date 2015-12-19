ff7f88233ebc148efc05a6fd434fb4b43aef7f7d
Kunal Nagpal <kunagpal@gmail.com>
2015-12-19 04:44:05 +0400

    Production check.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,10 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+
+if Rails.env == 'development'
+  Dotenv::Railtie.load
+end
 
 module FM
   class Application < Rails::Application
