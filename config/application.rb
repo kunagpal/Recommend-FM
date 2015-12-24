c7322473d1d4747c8bf2fcc5729cc08482402732
Kunal Nagpal <kunagpal@gmail.com>
2015-12-24 23:32:19 +0400

    API initiation.

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module FM
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.force_ssl = true
+
     config.generators do |g|
       g.orm :mongoid
     end
